25d0ef5d5d31d75c1c57449c81e4249cb294a4da
minseo23 <neverdie4757@gmail.com>
2021-10-17 15:51:15 +0900

    자바에서 제공하는 함수형 인터페이스

--- src/main/java/me/whiteship/java8to11/Foo.java
+++ src/main/java/me/whiteship/java8to11/Foo.java
@@ -1,17 +1,29 @@
 package me.whiteship.java8to11;
 
+import java.util.function.*;
+
 public class Foo {
 
     public static void main(String[] args) {
+//        Function<Integer, Integer> plus10 = (number) -> number + 10;
+//        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+//
+//        System.out.println(plus10.andThen(multiply2).apply(2));
 
-        int baseNumber = 10;
+//        Consumer<Integer> printT = (i) -> System.out.println(i);
+//        printT.accept(10);
 
-        RunSomething runSomething = new RunSomething() {
+//        Supplier<Integer> get10 = () -> 10;
+//
+//        System.out.println("get10 = " + get10);
 
-            @Override
-            public int doIt(int number) {
-                return number + baseNumber;
-            }
-        };
+//        Predicate<String> startsWithMinseo = (s) -> s.startsWith("minseo");
+//        Predicate<Integer> isEven = (i) -> i % 2 == 0;
+
+        UnaryOperator<Integer> plus10 = (i) -> i + 10;
+        UnaryOperator<Integer> multiply2 = (i) -> i * 2;
+
+        System.out.println(plus10.andThen(multiply2).apply(2));
+
     }
 }
