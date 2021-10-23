2b3b5033882a73cfbcd6a7ed78bc5eea68df5ec1
minseo23 <neverdie4757@gmail.com>
2021-10-23 15:43:16 +0900

    자바 8 API의 기본 메소드와 스태틱 메소드

--- src/main/java/me/whiteship/java8to11/App.java
+++ src/main/java/me/whiteship/java8to11/App.java
@@ -1,12 +1,26 @@
 package me.whiteship.java8to11;
 
+import java.util.ArrayList;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+
 public class App {
 
     public static void main(String[] args) {
-        Foo foo = new DefaultFoo("minseo");
-        foo.printName();
-        foo.printNameUpperCase();
+        ArrayList<Object> name = new ArrayList<>();
+        name.add("minseo");
+        name.add("double8");
+        name.add("toby");
+        name.add("foo");
 
-        Foo.printAnyThing();
+
+
+//        Spliterator<Object> spliterator = name.spliterator();
+//        Spliterator<Object> spliterator1 = spliterator.trySplit();
+//        while (spliterator.tryAdvance(System.out::println));
+//        System.out.println("=====================");
+//        while (spliterator1.tryAdvance(System.out::println));
+
+//        name.forEach(System.out::println);
     }
 }
